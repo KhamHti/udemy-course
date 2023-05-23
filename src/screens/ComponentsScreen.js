0e8bb32ab2e5593d97d8af88f8c29763dd00f159
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-24 00:02:05 +0630

    jsx variable

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,20 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const ComponentsScreen = () => {
+  const greeting = <Text style={styles.text}>Hello React Native</Text>;
   return (
     <View style={styles.container}>
       <Text>ComponentsScreen</Text>
+      {greeting}
     </View>
-  )
-}
+  );
+};
 
-export default ComponentsScreen
+export default ComponentsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+  },
+});
