43f7b764cf254184a71b39def655c718b734f846
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-24 01:02:14 +0630

    adding age

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,28 @@ import React from "react";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #234" },
-    { name: "Friend #3" },
-    { name: "Friend #5" },
+    { name: "Friend #1", age: 23 },
+    { name: "Friend #2", age: 345 },
+    { name: "Friend #3", age: 34 },
+    { name: "Friend #234", age: 12 },
+    { name: "Friend #3", age: 28 },
+    { name: "Friend #5", age: 33 },
   ];
 
   return (
     <View style={styles.container}>
-      <Text style={{marginBottom: 20}}>ListScreen</Text>
+      <Text style={{ marginBottom: 20 }}>ListScreen</Text>
       <FlatList
         data={friends}
         showsVerticalScrollIndicator={false}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => {
-          return <Text style={styles.textStyle}>{item.name}</Text>;
+          return (
+            <View style={{flexDirection: "row", marginLeft: 20, marginVertical: 30}}>
+              <Text style={{marginRight: 10, fontSize: 18}}>{item.name}:</Text>
+              <Text style={{marginLeft: 10, fontSize: 18}}>Age:{item.age}</Text>
+            </View>
+          );
         }}
       />
     </View>
@@ -33,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   textStyle: {
-    marginVertical: 50
-  }
+    marginVertical: 59,
+  },
 });
